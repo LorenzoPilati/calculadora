910883f28816985147e88197787f795fac31cf6b
LorenzoPrograming <lm.pilati@gmail.com>
2023-10-26 08:37:48 -0300

    calc

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,80 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  visor: number = 0;
+  visor: string = '0';
+  valor1!: number;
+  valor2!: number;
+  operacao!: number;
+  virgula: boolean = false;
+
   constructor() {}
 
+  adicionarNumero(num: string){
+    if(this.visor.length == 1 && this.visor == '0'){
+      this.visor = num;
+    }else{
+      this.visor += num;
+    }
+  }
+
+  adicionarOperacao(valor: number){
+    this.operacao = valor;
+    this.valor1 = +this.visor;
+    this.zerar();
+  }
+
+  adicionarVirgula(){
+    if(!this.virgula){
+      this.visor += '.';
+      this.virgula = true;
+    }
+  }
+
+  inverterSinal(){
+    if(this.visor != "0"){
+      if(this.visor[0] != '-'){
+        this.visor = "-" + this.visor;
+      }else{
+        this.visor = this.visor.slice(1);
+      }
+    }
+  }
+
+  calcular(){
+    this.valor2 = +this.visor;
+    switch(this.operacao){
+      case 0:{
+        this.visor = "" + (this.valor1 + this.valor2);
+        break;
+      }
+      case 1:{
+        this.visor = "" + (this.valor1 - this.valor2);
+        break;
+      }
+      case 2:{
+        this.visor = "" + (this.valor1 * this.valor2);
+        break;
+      }
+      case 3:{
+        if(this.valor2 != 0){
+          this.visor = "" + (this.valor1 / this.valor2);
+        }
+
+        break;
+      }
+      default:{
+        break;
+      }
+    }
+  }
+
+  porcentagem(){
+    this.valor2 = +this.visor;
+    this.visor = "" + (this.valor2 / 100);
+  }
+
+  zerar(){
+    this.visor = '0';
+    this.virgula = false;
+  }
 }
